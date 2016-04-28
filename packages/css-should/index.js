c6d1ad359db05b76ae64f88df73f5b89fd7a5787
Robin Pokorný <me@robinpokorny.com>
2016-04-28 11:47:22 +0200

    Add koa compose

--- packages/css-should/index.js
+++ packages/css-should/index.js
@@ -1,17 +1,21 @@
-import { parser } from 'css';
+import { parser } from 'css'
+import compose from 'koa-compose'
 
-export default function (css, html, plugins, options = {}) {
+import extractTests from './extract-tests'
+import toTAP from './to-tap'
+
+export default function (css, dom, plugins, options = {}) {
   if (typeof css === 'string') {
-    css = parse(css)
+    css = parser(css)
   }
 
-  const ast = compose(plugins.map({ preprocess } => preprocess))(css, options)
+  const ast = compose(plugins.map(({ preprocess }) => preprocess))({ css, options })
 
   const testList = extractTests(ast)
 
-  const runTests = compose(plugins.map({ tester } => tester))
+  const runTests = compose(plugins.map(({ tester }) => tester))
 
-  const result = testList.map((test) => runTests(test, css, html, options))
+  const result = testList.map((test) => runTests({ test, css, dom, options }))
 
   return toTAP(result)
 }
