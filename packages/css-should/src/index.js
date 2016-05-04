042fa57b8fa04dcd483dad26be5eff1fabfe7f85
Robin Pokorný <me@robinpokorny.com>
2016-05-04 16:06:17 +0200

    Add testing middleware

--- packages/css-should/src/index.js
+++ packages/css-should/src/index.js
@@ -3,13 +3,38 @@ import compose from 'koa-compose'
 
 const extractTests = (ctx, next) => {
   return next().then(() => {
-    ctx.results = ctx.css.rules
+    ctx.tests = ctx.css.rules
+      .filter((rule) => rule.type === 'rule')
+      .map((rule) => rule.declarations.filter(
+        (d) => d.property === 'should')
+      )
+      .reduce((p, a) => p.concat(a), [])
   })
+}
+
+function parseShouldDeclaration (declaration) {
+  var re = /([a-z-]+)\s*("([^"]*)")?/
+  var match = declaration.value.match(re)
+
+  declaration.axis = match[1]
+  declaration.param = match[3]
+
+  return declaration
+}
+
+const parseShould = (ctx, next) => {
+  ctx.css.rules.forEach((rule) => rule.declarations
+    .filter((d) => d.property === 'should')
+    .map(parseShouldDeclaration)
+  )
+  return next()
 }
 
 const match = ({ test, fail, pass }, next) => {
   const { elements, axis, param } = test
   if (axis !== 'match') next()
+
+  pass(test)
 
   const isPassing = elements.every((elem) => {
     if (!elem.matches(param)) {
@@ -41,20 +66,34 @@ export default function (css, document, emit, options = {}) {
   }
 
   const ctx = { css, options, document }
-  const fail = (test, err) => emit('fail', test, err)
-  const pass = (test) => emit('pass', test)
+  const fail = (test, err) => {
+    test.passed = false
+    emit('fail', test, err)
+  }
+  const pass = (test) => {
+    test.passed = true
+    emit('pass', test)
+  }
 
   const preprocessors = pluck('preprocess', plugins)
-  const tests = pluck('tests', plugins)
-  const testRunner = compose([ tests, match, notHandled ])
+  const tests = pluck('test', plugins)
+  const testRunner = compose([ ...tests, match, notHandled ])
 
   const middleware = [
     extractTests,
-    ...preprocessors
+    ...preprocessors,
+    parseShould
   ]
 
   return compose(middleware)(ctx)
-    .then(() => ctx.tests.forEach(
-      (test) => testRunner({ test, fail, pass })
+    .then(() => ctx.tests.map(
+      (test) => {
+        testRunner({test, fail, pass})
+        return test
+      }
     ))
+    .then((results) => {
+      emit('done')
+      return results
+    })
 }
--- packages/css-should/test/index.js
+++ packages/css-should/test/index.js
@@ -1,10 +1,39 @@
 /* eslint-env mocha */
-import { expect } from 'chai'
+import chai from 'chai'
+import sinon from 'sinon'
+import sinonChai from 'sinon-chai'
+
+chai.use(sinonChai)
+
+const { expect } = chai
 
 import should from '../'
 
 describe('CSS should', () => {
   it('should be a function', () => {
     expect(should).to.be.a('function')
+  })
+
+  it('should return results', () => {
+    should('', null, () => {}).then((result) =>
+      expect(result).to.be.a('array')
+    )
+  })
+
+  it('should emit "done" when all test have run', () => {
+    const emit = sinon.spy()
+
+    should('', null, emit).then(() =>
+      expect(emit).to.have.been.calledWith('done')
+    )
+  })
+
+  it('should dddd', () => {
+    const emit = (...args) => console.log(...args)
+
+    should('body { should: match "body" }', null, emit).then((results) => {
+      console.log(results)
+      expect(results).to.have.lengthOf(2)
+    })
   })
 })
